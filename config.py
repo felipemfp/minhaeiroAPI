688d8fff0d3edcdd6dcf8d74148f6d283323c470
Felipe Pontes <felipemfpontes@gmail.com>
2016-02-11 21:15:45 -0300

    Add env var

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import os
+
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = \
-    'postgres://zlixqehdpsvbej:oXqiiLrCAScuuJn63BjQavcNY5@ec2-107-20-242-191.compute-1.amazonaws.com:5432/dd3ccgea5jpa7v'
+SQLALCHEMY_DATABASE_URI = os.environ['MINHAEIRO_DB']
